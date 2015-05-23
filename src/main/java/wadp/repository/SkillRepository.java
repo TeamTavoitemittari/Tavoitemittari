0665bc5e0f4fe552f99c150d3132464289c00bbf
Miska Noponen <miska.noponen@gmail.com>
2015-05-23 17:33:21 +0300

    added css files for special buttons and page layout

--- src/main/java/wadp/repository/SkillRepository.java
+++ src/main/java/wadp/repository/SkillRepository.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package wadp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wadp.domain.Skill;
 
 /**
  *
  * @author villvirt
  */
-public class SkillRepository {
+public interface SkillRepository extends JpaRepository<Skill, Long>{
     
 }
