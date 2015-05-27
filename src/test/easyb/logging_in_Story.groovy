a1f24d4a00b591bb0823e0b05f34ef09dd7ad19f
Anna <a.luhtakanta@gmail.com>
2015-05-27 14:30:36 +0300

    easyb update, not working

--- src/test/easyb/logging_in_Story.groovy
+++ src/test/easyb/logging_in_Story.groovy
@@ -1,5 +1,5 @@
 import wadp.*
-import wadp.domain.*
+import wadp.domain.User;
 import wadp.service.*
 import wadp.auth.*;
 import org.openqa.selenium.*
@@ -8,15 +8,17 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 description 'User can log in with valid username/password-combination'
 
 before "create a new user", {
-    User us = new User();
+    User user = new User()
+    
     us.setEmail("example@gmail.com")
     us.setPassword("paprika99")
     us.setName("Esimerkki")
     us.setUserRole("oppilas");
+    userRepository.save(user)
 }
 
 scenario "user can login with correct password", {
-/*        given 'right view is selected', {
+        given 'right view is selected', {
         driver = new HtmlUnitDriver();
         driver.get("http://localhost:8080/");
         driver.getPageSource()
@@ -38,6 +40,7 @@ scenario "user can login with correct password", {
    
 }
 
+/*
 scenario "user can not login with incorrect password", {
     given 'right view is selected', {
         driver = new HtmlUnitDriver();
@@ -74,6 +77,16 @@ scenario "nonexistent user can not login to ", {
     then 'user will not be logged in to system', {
         driver.getPageSource().contains("EI OLE TUNNUSTA? EI HÄTÄÄ.").shouldBe true
         driver.getPageSource().contains("Minun kurssini").shouldBe false
-    }
-    */
+    }  
+}
+*/
+
+
+def createUser() {
+    //def user = new User()
+    us.setEmail("example@gmail.com")
+    us.setPassword("paprika99")
+    us.setName("Esimerkki")
+    us.setUserRole("oppilas");
+    userRepository.save(user)
 }
