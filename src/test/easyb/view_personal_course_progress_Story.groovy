3b240fb66550f4f3688c1aa74b15380251660c1a
Miska Noponen <miska.noponen@gmail.com>
2015-05-28 22:46:54 +0300

    added template for tavoitemittari-story

---
+++ src/test/easyb/view_personal_course_progress_Story.groovy
@@ -0,0 +1,23 @@
+import wadp.*
+import wadp.domain.*;
+import wadp.service.*
+import wadp.auth.*;
+import org.openqa.selenium.*
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+description 'Anyone can view the course goal view for a course'
+
+
+
+scenario "User can view the course demo for a course", {
+        given 'correct view is selected', {
+    }
+
+    when 'user looks at the page', {
+    }
+
+    then 'user will see the course goal view', {
+    } 
+   
+}
+
