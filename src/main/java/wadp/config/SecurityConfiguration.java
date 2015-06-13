55b0128372fe022fb17a3abc490a85b302fba1bf
oharkonsalo <oharkonsalo@yahoo.com>
2015-06-13 03:25:50 +0300

    manually integrated new course creation from branch newCourse Also security config for the methods

--- src/main/java/wadp/config/SecurityConfiguration.java
+++ src/main/java/wadp/config/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -14,6 +16,9 @@ import wadp.auth.UserAuthenticationProvider;
 
 @Configuration
 @EnableWebMvcSecurity
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled=true)
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
---
+++ src/main/java/wadp/domain/form/CourseForm.java
@@ -0,0 +1,43 @@
+package wadp.domain.form;
+
+
+import org.hibernate.validator.constraints.NotBlank;
+
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Form object for new user creation. Necessary fields are passed to UserService that will create User object that will
+ * be saved to database
+ */
+
+
+
+public class CourseForm {
+
+    
+    @NotNull(message="Nimeä ei voi jättää tyhjäksi!")
+    @NotBlank(message="Nimeä ei voi jättää tyhjäksi! !")
+    private String name;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+
+}
