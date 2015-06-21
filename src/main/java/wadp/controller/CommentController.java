944c2ae945324eef4b481b3991e38ae6fb414c49
Miska Noponen <miska.noponen@gmail.com>
2015-06-21 18:10:44 +0300

    added commentcontroller class

---
+++ src/main/java/wadp/controller/CommentController.java
@@ -0,0 +1,17 @@
+
+package wadp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.POST)
+    public void postComment(){
+        //TODO: handle the creation or updating of a comment
+    }
+    
+}
