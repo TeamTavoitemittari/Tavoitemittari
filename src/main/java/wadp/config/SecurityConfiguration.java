5dd3ce855f05c7cdfe72e31a75a35aeb5f678557
Miska Noponen <miska.noponen@gmail.com>
2015-08-09 23:16:25 +0300

    added functionality for user to report a forgotten password

--- src/main/java/wadp/config/SecurityConfiguration.java
+++ src/main/java/wadp/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/index","/loginerror", "/prototype", "/coursedemo", "/welcome", "/login", "/register",
+                .antMatchers("/index","/loginerror", "/prototype", "/coursedemo", "/welcome", "/login", "/register", "/passwordretrieval",
                         "/css*/**", "/js*/**", "/fonts*/**", "/images*/**").permitAll()
                 .anyRequest().authenticated();
 
