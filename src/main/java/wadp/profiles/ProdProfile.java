a5bb88630d5ecd4952aa542876fcac2e03a73c37
hojahoja <jari.honkonen@helsinki.fi>
2015-09-01 21:30:35 +0300

    ready for database use

--- src/main/java/wadp/profiles/ProdProfile.java
+++ src/main/java/wadp/profiles/ProdProfile.java
@@ -1,6 +1,6 @@
 package wadp.profiles;
 
-import java.net.URI;
+
 import java.net.URISyntaxException;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
@@ -33,7 +33,7 @@ public class ProdProfile {
         vendorAdapter.setGenerateDdl(true);
 
         factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPersistenceUnitName("production");
+        factory.setPersistenceUnitName("product");
         factory.setPackagesToScan("wadp.domain");
         factory.setDataSource(dataSource());
 
@@ -43,17 +43,12 @@ public class ProdProfile {
 
     @Bean
     public DataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setDriverClassName("org.postgresql.Driver");
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        basicDataSource.setUrl("jdbc:postgresql://localhost/postgres");
+        basicDataSource.setUsername("postgres");
+        basicDataSource.setPassword("testpg");
 
         return basicDataSource;
     }
